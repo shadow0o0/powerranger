2e158c1aa58394e5aec6eec198c507a0d9e7dd08
shadow0oo0 <37043722+shadow0oo0@users.noreply.github.com>
2018-06-11 12:25:44 +0300

    qweqwe
    qweqw

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
-
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('./guilds.sqlite');
 bot.commands = new Discord.Collection();
 
 
@@ -40,67 +41,16 @@ fs.readdir("./others/", (err, files) => {
 
 bot.on("ready", async () => {
   console.log(`bot is online`);
-  
+  const guilds = sql.prepare("SELECT count(*) FROM sqlite_master WHERE type='table' AND name = 'guilds';").get();
+  if (!guilds['count(*)']) {
+    // If the table isn't there, create it and setup the database correctly.
+    sql.prepare("CREATE TABLE guilds (guildID TEXT PRIMARY KEY, chname TEXT, catname TEXT);").run();
+  }
+  bot.getGuilds = sql.prepare("SELECT * FROM guilds");
+  bot.setGuilds = sql.prepare("INSERT OR REPLACE INTO guilds (guildID, chname, catname) VALUES (@guildID, @chname, @catname);");
 });
 
-bot.on('voiceStateUpdate',async (oldMember, newMember) => {
-    let guild = bot.guilds.get("358330224984719370");
-let newUserChannel = newMember.voiceChannel
-let oldUserChannel = oldMember.voiceChannel
 
-if(oldUserChannel === undefined && newUserChannel !== undefined) {
-
-    if(newMember.voiceChannel.id === '455001630258233354'){
-
-      await guild.createChannel(`${newMember.user.username}`,'voice').then(async (m)  => await m.overwritePermissions(newMember.user, {MANAGE_CHANNELS: true})).then(async (m)  => await m.setParent('454778578270420993')).then( async (m)  => await guild.members.get(newMember.user.id).setVoiceChannel(m))
-      guild.channels.find("id",`455001630258233354`).overwritePermissions(oldMember.user, {CONNECT: false})
-    }
-
-}if(newUserChannel === undefined){
-
-if(oldMember.voiceChannel.name === newMember.user.username){
-  
-  if(oldMember.voiceChannel.parentID === '454778578270420993'){
-     bot.setTimeout(() =>{
-         oldMember.voiceChannel.delete()
-         guild.channels.find("id",`455001630258233354`).overwritePermissions(oldMember.user,{CONNECT: null})
-    }, 3000)
-  }
-        
-}
-}if(oldMember.voiceChannel.name !== newMember.voiceChannel.name){
-  
-  if(oldMember.voiceChannel.parentID === '454778578270420993' && oldMember.voiceChannel.id !== '455001630258233354' && oldMember.voiceChannel.name === newMember.user.username){
-    bot.setTimeout(() =>{
-         oldMember.voiceChannel.delete()
-         guild.channels.find("id",`455001630258233354`).overwritePermissions(oldMember.user,{CONNECT: null})
-    }, 3000)
-  }
-        
-        }
-  if(newMember.voiceChannel.id === '455001630258233354'){
-
-   await guild.createChannel(`${newMember.user.username}`,'voice').then(async (m)  => await m.overwritePermissions(newMember.user, {MANAGE_CHANNELS: true})).then(async (m)  => await m.setParent('454778578270420993')).then( async (m)  => await guild.members.get(newMember.user.id).setVoiceChannel(m))
-    guild.channels.find("id",`455001630258233354`).overwritePermissions(oldMember.user, {CONNECT: false})
-  }
-})
-
-
-bot.on('channelUpdate', async  (oldChannel, newChannel) => {
-  let guild = bot.guilds.get("358330224984719370");
-  if(newChannel.name !== oldChannel.name){
-
-    if(oldChannel.parentID === '454778578270420993' || newChannel.parentID === '454778578270420993'){
-          const entry = await guild.fetchAuditLogs({type: 'CHANNEL_UPDATE'}).then(audit => audit.entries.first() )
-      let user = ""
-
-    user = entry.executor
-    if(user.bot)return;
-     return newChannel.setName(oldChannel.name)
-
-    }
-}
-})
 
 bot.on("message", async message => {
 if (message.author.bot) return;
@@ -113,7 +63,7 @@ if (message.channel.type ==="dm") return;
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
   let commandfile = bot.commands.get(cmd.slice(prefix.length));
-  if(commandfile) commandfile.run(bot,message,args);
+  if(commandfile) commandfile.run(bot,message,args,sql);
 
 
 });
